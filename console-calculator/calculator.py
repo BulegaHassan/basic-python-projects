26592e87689399f1655647ebcc9b4648f61e0ec0
BulegaHassan <hassanbulega@gmail.com>
2024-09-08 23:20:31 +0300

    get input from user

---
+++ console-calculator/calculator.py
@@ -0,0 +1,8 @@
+
+
+number1 = int(input('Please enter the first number : '))
+
+input("Enter the operation sign; + or - or * or / or %: ")
+
+number2 = int(input('Please enter the second number :  '))
+
