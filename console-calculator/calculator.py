6ba21852daf3a0d722bec154b70ae567edb05069
BulegaHassan <hassanbulega@gmail.com>
2024-09-09 16:56:50 +0300

    add operations and executea function

--- console-calculator/calculator.py
+++ console-calculator/calculator.py
@@ -1,8 +1,27 @@
 
+def pyCalculator() :
 
-number1 = int(input('Please enter the first number : '))
+ number1 = float(input('Please enter the first number : '))
 
-input("Enter the operation sign; + or - or * or / or %: ")
+ operator = input("Enter the operation sign; + or - or * or / : ")
 
-number2 = int(input('Please enter the second number :  '))
+ number2 = float(input('Please enter the second number :  '))
 
+ if operator == '+' :
+    result = number1 + number2
+ elif operator == '-' :
+    result = number1 - number2
+ elif operator == '*' :
+    result = number1 * number2
+ elif operator == '/' :
+    if number2 != 0:
+      result = number1 / number2
+    else:
+        return " Error! denominator can't be zero "
+ else :
+  return "Invalid operation "
+ 
+ return f'The result is {result}'
+
+
+print(pyCalculator())
