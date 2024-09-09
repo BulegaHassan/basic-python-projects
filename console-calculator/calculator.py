6ca9dae0e82f804842c9e391152633ca2f6edab1
BulegaHassan <hassanbulega@gmail.com>
2024-09-09 17:10:09 +0300

    format result Print colored text or bolded text accordingly

--- console-calculator/calculator.py
+++ console-calculator/calculator.py
@@ -1,4 +1,13 @@
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 def pyCalculator() :
 
  number1 = float(input('Please enter the first number : '))
@@ -17,11 +26,11 @@ def pyCalculator() :
     if number2 != 0:
       result = number1 / number2
     else:
-        return " Error! denominator can't be zero "
+        return f"{bcolors.FAIL} Error! denominator can't be zero "
  else :
-  return "Invalid operation "
+  return f"{bcolors.FAIL}Invalid operation "
  
- return f'The result is {result}'
+ return f'{bcolors.OKBLUE} {bcolors.BOLD}The result is {bcolors.UNDERLINE}{result}'
 
 
 print(pyCalculator())
